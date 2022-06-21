aafc1d8da04abd970773459cd2af2296c9669b1d
Youssef <choukriyoussef7905@gmail.com>
2022-06-21 13:56:38 +0100

    Link the display screen with oparands and operations

--- calculator.js
+++ calculator.js
@@ -1,3 +1,14 @@
+const operands = document.querySelectorAll('.operand');
+const operators = document.querySelectorAll('.operator');
+const displayScreen = document.querySelector('.display_screen');
+const operationDisplay = document.querySelector('.operation_display');
+const operatorDisplay = document.querySelector('.operator_display');
+
+let result;
+let operandA;
+let operandB;
+let currentOperation;
+
 function add(a, b) {
     return a + b;
 }
@@ -13,3 +24,15 @@ function substract(a, b) {
 function devide(a, b) {
     return a / b;
 }
+
+operands.forEach(operand => {
+    operand.addEventListener('click', () => {
+        operationDisplay.textContent += operand.textContent;
+    })
+})
+
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        operatorDisplay.textContent= operator.textContent; 
+    })
+})
